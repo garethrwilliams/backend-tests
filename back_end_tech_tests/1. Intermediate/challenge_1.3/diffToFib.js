9ecd26b72e19322342471ea0a5d96b32de102933
garethrwilliams <75028060+garethrwilliams@users.noreply.github.com>
2022-08-18 13:27:16 +0100

    challenge 1.3 complete

--- back_end_tech_tests/1. Intermediate/challenge_1.3/diffToFib.js
+++ back_end_tech_tests/1. Intermediate/challenge_1.3/diffToFib.js
@@ -1,3 +1,17 @@
-function diffToFib() {}
+function diffToFib(num) {
+  // Start the sequence
+  const fibArr = [0, 1];
+
+  //   If the first two nums appear return 0
+  if (fibArr.includes(num)) return 0;
+
+  //   Construct the sequence
+  while (fibArr[fibArr.length - 1] < num) {
+    fibArr.push(fibArr[fibArr.length - 1] + fibArr[fibArr.length - 2]);
+  }
+
+  //   Return the difference
+  return fibArr[fibArr.length - 1] - num;
+}
 
 module.exports = diffToFib;
