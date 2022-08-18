16a14a238872d6343ba7af223d13a16eb07ca16d
garethrwilliams <75028060+garethrwilliams@users.noreply.github.com>
2022-08-18 13:08:05 +0100

    adds the solution to the second test

--- back_end_tech_tests/1. Intermediate/challenge_1.2/isCoPrime.js
+++ back_end_tech_tests/1. Intermediate/challenge_1.2/isCoPrime.js
@@ -1,3 +1,25 @@
-const isCoPrime = (num1, num2) => {}
+const isCoPrime = (num1, num2) => {
+  // If they are the same then they can't be true
+  if (num1 === num2) return false;
+
+  //   Divisors of the lower number
+  const lowerNumDivisors = [];
+
+  //   Separate into low and high
+  const lowerNumber = num1 > num2 ? num2 : num1;
+  const higherNumber = lowerNumber === num1 ? num2 : num1;
+
+  // Extract the divisors from the lower number
+  for (let count = 2; count < lowerNumber; count++) {
+    if (!(lowerNumber % count)) {
+      lowerNumDivisors.push(count);
+    }
+  }
+
+  //   Compare with the higher number
+  return !lowerNumDivisors.some((num) => {
+    return !(higherNumber % num);
+  });
+};
 
 module.exports = isCoPrime;
