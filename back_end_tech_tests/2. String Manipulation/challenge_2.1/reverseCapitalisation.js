f3dc83173a38004b200f5d1d3baf697f3b8af89b
garethrwilliams <75028060+garethrwilliams@users.noreply.github.com>
2022-08-18 15:29:25 +0100

    challenge 2.1 complete

--- back_end_tech_tests/2. String Manipulation/challenge_2.1/reverseCapitalisation.js
+++ back_end_tech_tests/2. String Manipulation/challenge_2.1/reverseCapitalisation.js
@@ -1,3 +1,17 @@
-const reverseCapitalisation = () => {}
+const reverseCapitalisation = (str) => {
+  if (str.length === 0) return '';
+
+  return str
+    .split('')
+    .map((e) => {
+      console.log(e.charCodeAt(0));
+      if (e.charCodeAt(0) >= 65 && e.charCodeAt(0) <= 90)
+        return e.toLowerCase();
+      if (e.charCodeAt(0) >= 97 && e.charCodeAt(0) <= 122)
+        return e.toUpperCase();
+      return e;
+    })
+    .join('');
+};
 
 module.exports = reverseCapitalisation;
