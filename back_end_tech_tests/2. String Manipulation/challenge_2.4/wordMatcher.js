8a120cf7ffd36bb4ce4cdb6866298ccedcb860af
garethrwilliams <75028060+garethrwilliams@users.noreply.github.com>
2022-08-19 15:05:27 +0100

    challenge 2.4 complete

--- back_end_tech_tests/2. String Manipulation/challenge_2.4/wordMatcher.js
+++ back_end_tech_tests/2. String Manipulation/challenge_2.4/wordMatcher.js
@@ -1,3 +1,38 @@
-const wordMatcher = (matcher, str) => {}
+const wordMatcher = (matcher, str) => {
+  // If the matcher is more than one word reject the request
+  if (matcher.split(' ').length > 1) return [];
+
+  const returnArr = [];
+  //   Strip punctuation from the string and break into an array
+  const strArr = str.replace(/[\p{P}$+<=>^`|~]/gu, '').split(' ');
+
+  //   Look at each word and compare for match
+  strArr.forEach((word) => {
+    // If the word is too short or is not the same length as the matcher
+    if (word.length === 0 || word.length !== matcher.length) return;
+
+    let wordMatch = true;
+
+    for (let i = 0; i < matcher.length; i++) {
+      // If the letter doesn't match the letter with the exception of a ?
+      // If the word contains non-letters
+      if (
+        (matcher[i] !== word[i] && matcher[i] !== '?') ||
+        !/[A-Za-z]/.test(word[i])
+      )
+        return (wordMatch = false);
+    }
+    if (wordMatch) returnArr.push(word);
+  });
+
+  return returnArr;
+};
+
+console.log(
+  wordMatcher(
+    '??e?',
+    'leet, beat, seet, sethring, 99e9, Seep, blep, flem, M4er'
+  )
+);
 
 module.exports = wordMatcher;
