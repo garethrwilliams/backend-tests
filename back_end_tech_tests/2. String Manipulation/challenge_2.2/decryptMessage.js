742e88bfdfbe52f54f59fb7491b4fcf3e41a34b0
garethrwilliams <75028060+garethrwilliams@users.noreply.github.com>
2022-08-19 14:18:20 +0100

    challenge 3.1 complete

--- back_end_tech_tests/2. String Manipulation/challenge_2.2/decryptMessage.js
+++ back_end_tech_tests/2. String Manipulation/challenge_2.2/decryptMessage.js
@@ -5,8 +5,9 @@ const decryptMessage = (message, shift) => {
       // Get the lowercase character number
       let charCode = e.toLowerCase().charCodeAt(0);
 
-      //
+      // If the number is between a-z then shift the character
       if (charCode >= 97 && charCode <= 122) {
+        // Get the remainder of the shift is it is above the length of the alphabet
         charCode = charCode - (shift % 26);
         if (charCode < 97) charCode = 123 - (97 - charCode);
         if (charCode > 122) charCode = 97 + (charCode - 123);
--- back_end_tech_tests/3. Object Manipulation/challenge_3.1/areIdentical.js
+++ back_end_tech_tests/3. Object Manipulation/challenge_3.1/areIdentical.js
@@ -1,3 +1,34 @@
-function areIdentical(obj1, obj2) {}
+function areIdentical(obj1, obj2) {
+  const obj1Arr = Object.entries(obj1).flat();
+  const obj2Arr = Object.entries(obj2).flat();
+
+  if (obj1Arr.length !== obj2Arr.length) return false;
+
+  function flatten(into, node) {
+    if (node == null) return into;
+    if (typeof node === 'object' && !Array.isArray(node) && node !== null)
+      node = Object.entries(node);
+    if (Array.isArray(node)) return node.reduce(flatten, into);
+    into.push(node);
+    return flatten(into);
+  }
+
+  const obj1FlatArr = flatten([], obj1Arr);
+  const obj2FlatArr = flatten([], obj2Arr);
+
+  for (let i in obj1FlatArr) {
+    if (obj1FlatArr[i] !== obj2FlatArr[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(
+  areIdentical(
+    { 1: 1, 2: 2, 3: { 4: 4, 5: { 7: 7 } } },
+    { 1: 1, 2: 2, 3: { 4: 4, 5: { 7: 6 } } }
+  )
+);
 
 module.exports = areIdentical;
