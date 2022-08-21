65e661a0281a1abc1359e45bf40a3bdaaa070e73
garethrwilliams <75028060+garethrwilliams@users.noreply.github.com>
2022-08-21 15:46:53 +0100

    challenge 2.5 complete

--- back_end_tech_tests/2. String Manipulation/challenge_2.5/decodeBinary.js
+++ back_end_tech_tests/2. String Manipulation/challenge_2.5/decodeBinary.js
@@ -1,3 +1,37 @@
-function decodeBinary(binaryString) {}
+function decodeBinary(binaryString) {
+  // Create and instantiate variables sentence, value and letter
+  let sentence = '';
+  let value = 0;
+  let letter;
+
+  // Loop through the binary string
+  for (let i = 0; i < binaryString.length; i++) {
+    // For each position the 1's or 0's take, add the base 10 value to value
+    value += binaryString[i] * Math.pow(2, 4 - (i % 5));
+    console.log('Math.pow:', Math.pow(2, 4 - (i % 5)), i % 5);
+
+    // When the next iteration of the loop is divisible by 5, do the following...
+    if ((i + 1) % 5 === 0) {
+      console.log('value:', value);
+      // If the value is 27 set the next letter to a blank space, otherwise, using fromCharCode and value get the utf-8
+      // character (we need to add 96 as the "a" corresponds to 97)
+      value === 27
+        ? (letter = ' ')
+        : (letter = String.fromCharCode(96 + value));
+      // Add the letter to the sentence
+      sentence += letter;
+      // Reset the  value and letter components
+      value = 0;
+      letter = '';
+    }
+  }
+
+  // Return the decoded sentence
+  return sentence;
+}
+
+console.log((16).toString(2));
+
+console.log(decodeBinary('0100001001110110110100101'));
 
 module.exports = decodeBinary;
