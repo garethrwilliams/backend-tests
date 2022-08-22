97302b6408b299d6c286225b76f11ffcbfc55b76
garethrwilliams <75028060+garethrwilliams@users.noreply.github.com>
2022-08-22 14:15:34 +0100

    Completes 3.3

--- back_end_tech_tests/3. Object Manipulation/challenge_3.3/objFilter.js
+++ back_end_tech_tests/3. Object Manipulation/challenge_3.3/objFilter.js
@@ -1,3 +1,17 @@
-function objFilter () {}
+function objFilter(obj, func) {
+  // Do not mutate original
+  const returnObj = { ...obj };
+
+  // Iterate through the object
+  for (let key in returnObj) {
+    // If the function returns false delete the key
+    if (!func(returnObj[key])) {
+      delete returnObj[key];
+    }
+  }
+
+  // Return the object
+  return returnObj;
+}
 
 module.exports = objFilter;
