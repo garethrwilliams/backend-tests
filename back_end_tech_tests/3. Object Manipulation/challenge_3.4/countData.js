385a7359dda681608bd419a37d8dabaf861d37e7
garethrwilliams <75028060+garethrwilliams@users.noreply.github.com>
2022-08-22 15:25:49 +0100

    completes 3.4

--- back_end_tech_tests/3. Object Manipulation/challenge_3.4/countData.js
+++ back_end_tech_tests/3. Object Manipulation/challenge_3.4/countData.js
@@ -1,3 +1,28 @@
-function countData () {}
+function countData(obj, returnCount) {
+  // Return count is intitilised or loaded
+  returnCount = returnCount ? returnCount : {};
+
+  // Helper function
+  function updateReturnCount(data) {
+    returnCount[data] = returnCount[data] ? ++returnCount[data] : 1;
+  }
+
+  // Iterate through and log data types
+  for (let key in obj) {
+    if (Array.isArray(obj[key])) {
+      updateReturnCount('array');
+
+      countData(obj[key], returnCount);
+    } else if (typeof obj[key] === 'object') {
+      updateReturnCount(typeof obj[key]);
+
+      countData(obj[key], returnCount);
+    } else {
+      updateReturnCount(typeof obj[key]);
+    }
+  }
+
+  return returnCount;
+}
 
 module.exports = countData;
